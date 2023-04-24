1f0b9ef79c5685b5fdc07fbbbc06a65318ac1f34
sashazakatov <58004989+sashazakatov@users.noreply.github.com>
2023-04-24 12:34:44 +0300

    adds ImageGallery and ImageGalleryItem

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,9 @@
+import css from './ImageGallery.module.css'
+import {ImageGalleryItem} from './ImageGallery.module.css'
+
+export const ImageGallery = () => {
+    return(
+        <ul className={css.ImageGallery}>
+        </ul>
+    )
+}
